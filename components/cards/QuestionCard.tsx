6cbbb4629a4bc87f1c395bd040c15bc26505a3a3
Gurvinder Singh <gurvindermaksudra@gmail.com>
2024-03-12 02:29:39 +0530

    show the saved

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,8 +1,8 @@
-import Link from 'next/link';
-import React from 'react'
-import RenderTag from '../shared/RenderTag';
-import Metric from '../shared/Metric';
-import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
+import Link from "next/link";
+import React from "react";
+import RenderTag from "../shared/RenderTag";
+import Metric from "../shared/Metric";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
 
 interface QuestionProps {
   _id: string;
@@ -16,7 +16,7 @@ interface QuestionProps {
     name: string;
     picture: string;
   };
-  upvotes: number;
+  upvotes: string[];
   views: number;
   answers: Array<object>;
   createdAt: Date;
@@ -30,7 +30,7 @@ const QuestionCard = ({
   upvotes,
   views,
   answers,
-  createdAt
+  createdAt,
 }: QuestionProps) => {
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
@@ -41,14 +41,14 @@ const QuestionCard = ({
           </span>
           <Link href={`/question/${_id}`}>
             <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
-              {title} 
+              {title}
             </h3>
           </Link>
         </div>
 
         {/* If signed in add edit delete actions */}
       </div>
-      
+
       <div className="mt-3.5 flex flex-wrap gap-2">
         {tags.map((tag) => (
           <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
@@ -56,41 +56,40 @@ const QuestionCard = ({
       </div>
 
       <div className="flex-between mt-6 w-full flex-wrap gap-3">
-          <Metric 
-            imgUrl={author.picture}
-            alt="user"
-            value={author.name}
-            title={` - asked ${getTimestamp(createdAt)}`}
-            href={`/profile/${author._id}`}
-            isAuthor
-            textStyles="body-medium text-dark400_light700"
-          />
+        <Metric
+          imgUrl={author.picture}
+          alt="user"
+          value={author.name}
+          title={` - asked ${getTimestamp(createdAt)}`}
+          href={`/profile/${author._id}`}
+          isAuthor
+          textStyles="body-medium text-dark400_light700"
+        />
 
-          <Metric 
-            imgUrl="/assets/icons/like.svg"
-            alt="Upvotes"
-            value={formatAndDivideNumber(upvotes)}
-            title=" Votes"
-            textStyles="small-medium text-dark400_light800"
-          />
-          <Metric 
-            imgUrl="/assets/icons/message.svg"
-            alt="message"
-            value={formatAndDivideNumber(answers.length)}
-            title=" Answers"
-            textStyles="small-medium text-dark400_light800"
-          />
-          <Metric 
-            imgUrl="/assets/icons/eye.svg"
-            alt="eye"
-            value={formatAndDivideNumber(views)}
-            title=" Views"
-            textStyles="small-medium text-dark400_light800"
-          />
+        <Metric
+          imgUrl="/assets/icons/like.svg"
+          alt="Upvotes"
+          value={formatAndDivideNumber(upvotes.length)}
+          title=" Votes"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="message"
+          value={formatAndDivideNumber(answers.length)}
+          title=" Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="eye"
+          value={formatAndDivideNumber(views)}
+          title=" Views"
+          textStyles="small-medium text-dark400_light800"
+        />
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default QuestionCard
+export default QuestionCard;
