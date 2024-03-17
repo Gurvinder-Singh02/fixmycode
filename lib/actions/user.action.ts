df46f1a313f1666e8d384b5bddb7f23ca66e6c06
Gurvinder Singh <gurvindermaksudra@gmail.com>
2024-03-17 19:39:47 +0530

    added pagination

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -90,7 +90,8 @@ export async function getAllUsers(params: GetAllUsersParams) {
   try {
     connectToDatabase();
 
-    const { searchQuery, filter } = params;
+    const { searchQuery, filter, page = 1, pageSize = 10 } = params;
+    const skipAmount = (page - 1) * pageSize;
 
     const query: FilterQuery<typeof User> = {};
 
@@ -119,9 +120,14 @@ export async function getAllUsers(params: GetAllUsersParams) {
     }
 
     const users = await User.find(query)
-      .sort(sortOptions);
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize)
 
-    return { users };
+    const totalUsers = await User.countDocuments(query);
+    const isNext = totalUsers > skipAmount + users.length;
+
+    return { users, isNext };
   } catch (error) {
     console.log(error);
     throw error;
@@ -167,7 +173,9 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
   try {
     connectToDatabase();
 
-    const { clerkId, searchQuery, filter } = params;
+    const { clerkId, searchQuery, filter, page = 1, pageSize = 2 } = params;
+
+    const skipAmount = (page - 1) * pageSize;
     
     const query: FilterQuery<typeof Question> = searchQuery
       ? { title: { $regex: new RegExp(searchQuery, 'i') } }
@@ -196,11 +204,15 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
           break;
       }
 
-    const user = await User.findOne({ clerkId }).populate({
+    const user = await User
+    .findOne({ clerkId })
+    .populate({
       path: 'saved',
       match: query,
       options: {
         sort: sortOptions,
+        skip: skipAmount,
+        limit: pageSize + 1,
       },
       populate: [
         { path: 'tags', model: Tag, select: "_id name" },
@@ -208,13 +220,15 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
       ]
     })
 
+    const isNext = user.saved.length > pageSize;
+    
     if(!user) {
       throw new Error('User not found');
     }
 
     const savedQuestions = user.saved;
 
-    return { questions: savedQuestions };
+    return { questions: savedQuestions, isNext };
   } catch (error) {
     console.log(error);
     throw error;
@@ -251,16 +265,22 @@ export async function getUserQuestions(params: GetUserStatsParams) {
   try {
     connectToDatabase();
 
-    const { userId, page = 1, pageSize = 10 } = params;
+    const { userId, page = 1, pageSize = 2 } = params;
+
+    const skipAmount = (page - 1) * pageSize;
 
     const totalQuestions = await Question.countDocuments({ author: userId})
 
     const userQuestions = await Question.find({ author: userId })
       .sort({ views: -1, upvotes: -1 })
+      .skip(skipAmount)
+      .limit(pageSize)
       .populate('tags', '_id name')
       .populate('author', '_id clerkId name picture')
 
-    return { totalQuestions, questions: userQuestions };
+      const isNextQuestions = totalQuestions > skipAmount + userQuestions.length;
+
+    return { totalQuestions, questions: userQuestions, isNextQuestions };
   } catch (error) {
     console.log(error);
     throw error;
@@ -271,16 +291,22 @@ export async function getUserAnswers(params: GetUserStatsParams) {
   try {
     connectToDatabase();
 
-    const { userId, page = 1, pageSize = 10 } = params;
+    const { userId, page = 1, pageSize = 1 } = params;
+
+    const skipAmount = (page - 1) * pageSize;
 
     const totalAnswers = await Answer.countDocuments({ author: userId})
 
     const userAnswers = await Answer.find({ author: userId })
       .sort({ upvotes: -1 })
+      .skip(skipAmount)
+      .limit(pageSize)
       .populate('question', '_id title')
       .populate('author', '_id clerkId name picture')
+
+      const isNextAnswer = totalAnswers > skipAmount + userAnswers.length;
       
-    return { totalAnswers, answers: userAnswers };
+    return { totalAnswers, answers: userAnswers, isNextAnswer };
   } catch (error) {
     console.log(error);
     throw error;
