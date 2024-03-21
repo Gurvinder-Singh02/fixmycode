ca0a1c8e29b7bf1e9fc7cd937d29a5cb33e72ff6
Gurvinder Singh <gurvindermaksudra@gmail.com>
2024-03-22 02:54:25 +0530

    final V1

--- components/shared/search/GlobalResult.tsx
+++ components/shared/search/GlobalResult.tsx
@@ -71,7 +71,7 @@ const GlobalResult = () => {
 
         {isLoading ? (
           <div className="flex-center flex-col px-5">
-            <ReloadIcon className="my-2 h-10 w-10 animate-spin text-primary-500" />
+            <ReloadIcon className="my-2 size-10 animate-spin text-primary-500" />
             <p className="text-dark200_light800 body-regular">Browsing the entire database</p>
           </div>
         ): (
