b20be057a3103830a24c2c3adf8dc436d38fd283
Gurvinder Singh <gurvindermaksudra@gmail.com>
2024-05-11 17:08:47 +0530

    dev ready

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex min-h-screen w-full items-center justify-center">
+      <div className="absolute flex min-h-screen w-full items-center justify-center ">
+        <Image
+          src="/assets/images/LockBg.svg"
+          alt="profile picture"
+          width={1000}
+          height={1000}
+        />
+      </div>
       {children}
+      <p className="fixed bottom-4 text-sm opacity-35">
+        Copyright Owner &copy; Gurvinder singh
+      </p>
     </main>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
