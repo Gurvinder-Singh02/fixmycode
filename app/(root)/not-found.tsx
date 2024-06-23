a86ba30f82a448de6f05ce1616607c3f1b728c1e
Gurvinder Singh <gurvindermaksudra@gmail.com>
2024-06-23 19:15:15 +0530

    jobs update s

---
+++ app/(root)/not-found.tsx
@@ -0,0 +1,12 @@
+import NoResult from "@/components/shared/NoResult";
+
+export default function NotFound() {
+  return (
+    <NoResult
+      title="This Features will be Comming soon "
+      description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+      link="/ask-question"
+      linkTitle="Ask a Question"
+    />
+  );
+}
